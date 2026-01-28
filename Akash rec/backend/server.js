615a5da46dab32613401e72c07664dafeb7a64a2
akashaakashji <aav077075@gmail.com>
2026-01-28 14:47:52 +0530

    Refactor server.js for app export and local start
    Refactor server.js to export app and conditionally start server

--- Akash rec/backend/server.js
+++ Akash rec/backend/server.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 import driveRoutes from "./routes/driveRoutes.js";
 
 
@@ -11,11 +11,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"));
+//mongoose.connect(process.env.MONGO_URI)
+//.then(() => console.log("MongoDB Connected"));
 
 app.use("/api/drive", driveRoutes);
 
+
+export default app;
+
+//local only ka liya
+if (!process.env.VERCEL) {
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+}
